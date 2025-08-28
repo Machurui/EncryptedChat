8516f7b0928771eb8f41b0f9b11fbf531fbdd687
Machurui <francois.serra00@gmail.com>
2025-08-28 17:05:14 +0200

    Resolving branch

--- EncryptedChat/Models/Auth/ResendConfirmationEmailDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace EncryptedChat.Models;
-using System.ComponentModel.DataAnnotations;
-
-public class ResendConfirmationEmailDTO
-{
-    [Required]
-    public string Email { get; set; } = string.Empty;
-}
